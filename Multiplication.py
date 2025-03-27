7f237bb3b870eda320e1a31da5491cc1dd82c694
Dimitri Cromarty <dimitricromarty@Dimitris-MacBook-Air.local>
2025-03-27 12:12:59 -0400

    Waw where to even begin, Stayed up the entire night working on this not too sure if OPT Signed work but the rest work

--- Multiplication.py
+++ Multiplication.py
@@ -7,11 +7,13 @@ Product = "00000000"
 
 #Lets create a script for the multiplication hardware shouldnt be too bad
 
-def unsigned(Multiplier, Multiplicand, Product):
+def unsigned(Multiplier, Multiplicand, Product,mplier_len, mplcnd_len, prod_len ):
     
     mplier = int(Multiplier, 2)
     mplcnd = int(Multiplicand, 2)
     prod = int(Product, 2)
+    
+    
     
     if mplier & 1 == 1:
         prod = prod + mplcnd
@@ -19,21 +21,29 @@ def unsigned(Multiplier, Multiplicand, Product):
     mplier >>= 1
     mplcnd <<= 1
     
-    Multiplier = format(mplier, f'0{len(Multiplier)}b')
-    Multiplicand = format(mplcnd, f'0{len(Multiplicand)}b')
-    Product = format(prod, f'0{len(Product)}b')
+    Multiplier = format(mplier, f'0{mplier_len}b')[-mplier_len:]
+    Multiplicand = format(mplcnd, f'0{mplcnd_len}b')[-mplcnd_len:]
+    Product = format(prod, f'0{prod_len}b')[-prod_len:]
     
-    #print(Multiplicand, Multiplier, Product)
+    print(Multiplicand, Multiplier, Product)
     return Multiplier, Multiplicand, Product
 
 def main():
+    
+    
     Multiplicand = input("Please input the Multiplicand: ")
     Multiplier = input("Please input the Multiplier: ")
     number_of_loops = int(input("Please input which step you would like to stop at: "))
-    result = unsigned(Multiplier, Multiplicand, Product)
+    
+    mplier_len = len(Multiplier)
+    mplcnd_len = len(Multiplicand)
+    prod_len = mplcnd_len + mplier_len
+    mplcnd_len = prod_len
+    
+    result = unsigned(Multiplier, Multiplicand, Product, mplier_len, mplcnd_len, prod_len)
     for step_count in range(number_of_loops - 1):
-        result = unsigned(result[0], result[1], result[2])
-    print(result)
+        result = unsigned(result[0], result[1], result[2], mplier_len, mplcnd_len, prod_len)
+    print(result[:3])
         
         
 
